7db58acfb37f8ee5a4f5800cdf0f54228eb02c2e
Lixun Zhang <lixun.zhang@amd.com>
2023-07-20 23:08:59 -0500

    Print two tables at the same time

--- benchmarks/benchmark_flash_attention_forward.py
+++ benchmarks/benchmark_flash_attention_forward.py
@@ -38,17 +38,18 @@ def attention_ref(qkv, attn_mask, dropout_p, upcast=False, causal=False):
 
 torch.manual_seed(0)
 repeats = 20
-batch_size = [1,32,48,64,128]
-nheads = 16
-seqlen = [1024,2048,4096]
-n = 1024
+
+## table 1
+print('fwd-bs4-nheads48-d64-causal=False')
+batch_size = [4]
+nheads = 48
+seqlen = [1024,2048,4096,8192,16384]
+n = 3072
 d = n // nheads # 64
 dropout_p = 0.1
 causal = False
 dtype = torch.float16
 device = 'cuda'
-
-result_summary = []
 
 for bs in batch_size:
     for sq in seqlen:
@@ -68,11 +69,52 @@ for bs in batch_size:
                               h=nheads).detach().requires_grad_()
         qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
 
-        #print(f'Batch size: {bs}, Sequence Length: {sq}')
+        q, k, v = qkv.unbind(dim=2)
+        q = torch.transpose(q, 1, 2)
+        k = torch.transpose(k, 1, 2)
+        v = torch.transpose(v, 1, 2)
+        sm_scale = q.shape[-1] ** (-0.5)
+        qkv_triton = torch.stack([q, k, v], dim=2)
 
-        # fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
-        #     qkv_unpad, cu_sqs, max_sq_in_batch, dropout_p, causal=causal
-        # )
+        ## Triton FA implementation
+        fn = lambda flash_triton:flash_attn_triton(q, k, v, causal, 1.3)
+        fa_time,fa_measurement = benchmark_forward(fn, qkv_triton, repeats=20, desc='FlashAttention triton', verbose=False)
+
+        ## Measure time and compute tflops
+        triton_time = fa_measurement.mean;
+        flops_per_matmul = 2. * bs * nheads * sq * sq * d
+        total_flops = 2 * flops_per_matmul
+        if causal:
+            total_flops *= .5
+        triton_tflops = total_flops / triton_time * 1e-12
+        print(f'{bs:3d}  {sq:10d} {triton_tflops:.2f} tflops {triton_time*1e3:.3f} ms')
+
+## table 2
+print('fwd-nheads16-d64-causal=False')
+batch_size = [1,32,48,64,128]
+nheads = 16
+seqlen = [1024,2048,4096]
+n = 1024
+d = n // nheads # 64
+
+
+for bs in batch_size:
+    for sq in seqlen:
+        if (bs > 32 and sq > 2048) or (bs > 64 and sq > 1024):
+            continue
+        x = torch.randn(bs, sq, n, device='cuda', dtype=dtype, requires_grad=True)
+        Wqkv = torch.nn.Linear(nheads * d, 3 * nheads * d, device=device, dtype=dtype)
+
+        lengths = torch.randint(sq - 20, sq, (bs, 1), device='cuda')
+        attention_mask_bool = repeat(torch.arange(sq, device='cuda'), 's -> b s', b=bs) < lengths
+        attention_mask = torch.zeros(bs, sq, device='cuda', dtype=dtype)
+        attention_mask[~attention_mask_bool] = -10000.0
+        attention_mask = rearrange(attention_mask, 'b s -> b 1 1 s')
+
+        x_unpad, indices, cu_sqs, max_sq_in_batch = unpad_input(x, attention_mask_bool)
+        qkv_unpad = rearrange(Wqkv(x_unpad), 'nnz (t h d) -> nnz t h d', t=3,
+                              h=nheads).detach().requires_grad_()
+        qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
 
         q, k, v = qkv.unbind(dim=2)
         q = torch.transpose(q, 1, 2)
@@ -85,18 +127,6 @@ for bs in batch_size:
         fn = lambda flash_triton:flash_attn_triton(q, k, v, causal, 1.3)
         fa_time,fa_measurement = benchmark_forward(fn, qkv_triton, repeats=20, desc='FlashAttention triton', verbose=False)
 
-        ## Pytorch standard FA implementation
-        # fn = lambda qkv: attention_ref(qkv, attention_mask_bool, dropout_p, causal=causal)
-        ## CK FA implementation
-        # fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
-        #     qkv_unpad, cu_sqs, max_sq_in_batch, dropout_p, causal=causal
-        # )
-        # pyt_time,pyt_measurement = benchmark_forward(fn, qkv_unpad, repeats=repeats, desc='CK Attention')
-
-        # relative_perf = ((pyt_measurement.mean-fa_measurement.mean)/pyt_measurement.mean) * 100
-
-        # result_summary.append([bs,sq,pyt_measurement.mean,fa_measurement.mean,relative_perf])
-
         ## Measure time and compute tflops
         triton_time = fa_measurement.mean;
         flops_per_matmul = 2. * bs * nheads * sq * sq * d
@@ -105,9 +135,4 @@ for bs in batch_size:
             total_flops *= .5
         triton_tflops = total_flops / triton_time * 1e-12
         print(f'{bs:3d}  {sq:10d} {triton_tflops:.2f} tflops {triton_time*1e3:.3f} ms')
-        result_summary.append([bs,sq,triton_time*1e3])
 
-        # print(f'Flash Attention Speedup: {relative_perf}\n')
-
-# print(f'batch size, sequence length, PyTorch Standard Attention, FlashAttention, speedup relative to PyTorch\n {result_summary}')
-#print(f'batch size, sequence length, Triton FlashAttention\n {result_summary}')
