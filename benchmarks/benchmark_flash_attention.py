8f92639fb83dd98c6c4571397837067a07ee2fec
Vinayak <Vinayak.Gokhale@amd.com>
2023-08-17 15:25:01 +0000

    Add ability to loop over different batch / seqlen

--- benchmarks/benchmark_flash_attention.py
+++ benchmarks/benchmark_flash_attention.py
@@ -37,34 +37,73 @@ def attention_ref(qkv, attn_mask, dropout_p, upcast=False, causal=False):
 
 
 torch.manual_seed(0)
-repeats = 30
-batch_size = 64
-nheads = 16
-seqlen = 1024
-n = 1024
-d = n // nheads
+repeats = 2
 dropout_p = 0.1
 causal = False
 dtype = torch.float16
 device = 'cuda'
 
-x = torch.randn(batch_size, seqlen, n, device='cuda', dtype=dtype, requires_grad=True)
-Wqkv = torch.nn.Linear(nheads * d, 3 * nheads * d, device=device, dtype=dtype)
+batch_size = [4]
+nheads = 48
+seqlen = [1024,2048,4096,8192,16384]
+n = 3072
+d = n // nheads
 
-lengths = torch.randint(seqlen - 20, seqlen, (batch_size, 1), device='cuda')
-attention_mask_bool = repeat(torch.arange(seqlen, device='cuda'), 's -> b s', b=batch_size) < lengths
-attention_mask = torch.zeros(batch_size, seqlen, device='cuda', dtype=dtype)
-attention_mask[~attention_mask_bool] = -10000.0
-attention_mask = rearrange(attention_mask, 'b s -> b 1 1 s')
+for bs in batch_size:
+    for sq in seqlen:
+        if (bs > 32 and sq > 2048) or (bs > 64 and sq > 1024):
+           continue
+        print(f'Batch / seq = {bs} / {sq}')
+        x = torch.randn(bs, sq, n, device='cuda', dtype=dtype, requires_grad=True)
+        Wqkv = torch.nn.Linear(nheads * d, 3 * nheads * d, device=device, dtype=dtype)
 
-x_unpad, indices, cu_seqlens, max_seqlen_in_batch = unpad_input(x, attention_mask_bool)
-qkv_unpad = rearrange(Wqkv(x_unpad), 'nnz (t h d) -> nnz t h d', t=3,
-                      h=nheads).detach().requires_grad_()
-qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
+        lengths = torch.randint(sq - 20, sq, (bs, 1), device='cuda')
+        attention_mask_bool = repeat(torch.arange(sq, device='cuda'), 's -> b s', b=bs) < lengths
+        attention_mask = torch.zeros(bs, sq, device='cuda', dtype=dtype)
+        attention_mask[~attention_mask_bool] = -10000.0
+        attention_mask = rearrange(attention_mask, 'b s -> b 1 1 s')
 
-fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
-    qkv_unpad, cu_seqlens, max_seqlen_in_batch, dropout_p, causal=causal
-)
-benchmark_all(fn, qkv_unpad, repeats=repeats, desc='FlashAttention')
-fn = lambda qkv: attention_ref(qkv, attention_mask_bool, dropout_p, causal=causal)
-benchmark_all(fn, qkv, repeats=repeats, desc='PyTorch Standard Attention')
+        x_unpad, indices, cu_seqlens, max_seqlen_in_batch = unpad_input(x, attention_mask_bool)
+        qkv_unpad = rearrange(Wqkv(x_unpad), 'nnz (t h d) -> nnz t h d', t=3,
+                              h=nheads).detach().requires_grad_()
+        qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
+
+        fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
+            qkv_unpad, cu_seqlens, max_seqlen_in_batch, dropout_p, causal=causal
+        )
+        benchmark_all(fn, qkv_unpad, repeats=repeats, desc='FlashAttention')
+        #fn = lambda qkv: attention_ref(qkv, attention_mask_bool, dropout_p, causal=causal)
+        #benchmark_all(fn, qkv, repeats=repeats, desc='PyTorch Standard Attention')
+
+
+batch_size = [4,32,48,64,128]
+nheads = 16
+seqlen = [1024,2048,4096]
+n = 1024
+d = n // nheads
+
+for bs in batch_size:
+    for sq in seqlen:
+        if (bs > 32 and sq > 2048) or (bs > 64 and sq > 1024):
+           continue
+        print(f'Batch / seq = {bs} / {sq}')
+        x = torch.randn(bs, sq, n, device='cuda', dtype=dtype, requires_grad=True)
+        Wqkv = torch.nn.Linear(nheads * d, 3 * nheads * d, device=device, dtype=dtype)
+
+        lengths = torch.randint(sq - 20, sq, (bs, 1), device='cuda')
+        attention_mask_bool = repeat(torch.arange(sq, device='cuda'), 's -> b s', b=bs) < lengths
+        attention_mask = torch.zeros(bs, sq, device='cuda', dtype=dtype)
+        attention_mask[~attention_mask_bool] = -10000.0
+        attention_mask = rearrange(attention_mask, 'b s -> b 1 1 s')
+
+        x_unpad, indices, cu_seqlens, max_seqlen_in_batch = unpad_input(x, attention_mask_bool)
+        qkv_unpad = rearrange(Wqkv(x_unpad), 'nnz (t h d) -> nnz t h d', t=3,
+                              h=nheads).detach().requires_grad_()
+        qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
+
+        fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
+            qkv_unpad, cu_seqlens, max_seqlen_in_batch, dropout_p, causal=causal
+        )
+        benchmark_all(fn, qkv_unpad, repeats=repeats, desc='FlashAttention')
+        #fn = lambda qkv: attention_ref(qkv, attention_mask_bool, dropout_p, causal=causal)
+        #benchmark_all(fn, qkv, repeats=repeats, desc='PyTorch Standard Attention')
