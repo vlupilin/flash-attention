56aa3602617a0bd3c3b34fb89d2a88e10add9b76
guangzlu <guangzlu@amd.com>
2024-04-19 08:53:10 +0000

    delete unused

--- benchmarks/benchmark_flash_attention_308.py
+++ benchmarks/benchmark_flash_attention_308.py
@@ -33,35 +33,6 @@ def efficiency(flop, time):
     return (flop / time / 10**12) if not math.isnan(time) else 0.0
 
 
-def attention_pytorch(qkv, dropout_p=0.0, causal=True):
-    """
-    Arguments:
-        qkv: (batch_size, seqlen, 3, nheads, head_dim)
-        dropout_p: float
-    Output:
-        output: (batch_size, seqlen, nheads, head_dim)
-    """
-    batch_size, seqlen, _, nheads, d = qkv.shape
-    q, k, v = qkv.unbind(dim=2)
-    q = rearrange(q, 'b t h d -> (b h) t d')
-    k = rearrange(k, 'b s h d -> (b h) d s')
-    softmax_scale = 1.0 / math.sqrt(d)
-    # Preallocate attn_weights for `baddbmm`
-    scores = torch.empty(batch_size * nheads, seqlen, seqlen, dtype=qkv.dtype, device=qkv.device)
-    scores = rearrange(torch.baddbmm(scores, q, k, beta=0, alpha=softmax_scale),
-                       '(b h) t s -> b h t s', h=nheads)
-    if causal:
-        # "triu_tril_cuda_template" not implemented for 'BFloat16'
-        # So we have to construct the mask in float
-        causal_mask = torch.triu(torch.full((seqlen, seqlen), -10000.0, device=scores.device), 1)
-        # TD [2022-09-30]: Adding is faster than masked_fill_ (idk why, just better kernel I guess)
-        scores = scores + causal_mask.to(dtype=scores.dtype)
-    attention = torch.softmax(scores, dim=-1)
-    attention_drop = F.dropout(attention, dropout_p)
-    output = torch.einsum('bhts,bshd->bthd', attention_drop , v)
-    return output.to(dtype=qkv.dtype)
-
-
 def time_fwd_bwd(func, *args, **kwargs):
     time_f, time_b = benchmark_fwd_bwd(func, *args, **kwargs)
     return time_f[1].mean, time_b[1].mean
@@ -99,48 +70,6 @@ for causal in causal_vals:
             )
             time_f[config, "Flash2"] = f
             time_b[config, "Flash2"] = b
-
-            try:
-                qkv = qkv.detach().requires_grad_(True)
-                f, b = time_fwd_bwd(
-                    attention_pytorch, qkv, dropout_p, causal=causal, repeats=repeats, verbose=False
-                )
-            except:  # Skip if OOM
-                f, b = float('nan'), float('nan')
-            time_f[config, "Pytorch"] = f
-            time_b[config, "Pytorch"] = b
-
-            if attention_triton is not None:
-                q, k, v = [torch.randn(batch_size, nheads, seqlen, headdim, device=device, dtype=dtype,
-                                    requires_grad=True) for _ in range(3)]
-                # Try both values of sequence_parallel and pick the faster one
-                try:
-                    f, b = time_fwd_bwd(
-                        attention_triton, q, k, v, causal, headdim**(-0.5),
-                        False, repeats=repeats, verbose=False
-                    )
-                except:
-                    f, b = float('nan'), float('inf')
-                try:
-                    _, b0 = time_fwd_bwd(
-                        attention_triton, q, k, v, causal, headdim**(-0.5),
-                        True, repeats=repeats, verbose=False
-                    )
-                except:
-                    b0 = float('inf')
-                time_f[config, "Triton"] = f
-                time_b[config, "Triton"] = min(b, b0) if min(b, b0) < float('inf') else float('nan')
-
-            if xops is not None:
-                q, k, v = [torch.randn(batch_size, seqlen, nheads, headdim, device=device, dtype=dtype,
-                                    requires_grad=True) for _ in range(3)]
-                f, b = time_fwd_bwd(
-                    xops.memory_efficient_attention, q, k, v,
-                    attn_bias=xops.LowerTriangularMask() if causal else None,
-                    op=(xops.fmha.cutlass.FwOp, xops.fmha.cutlass.BwOp)
-                )
-                time_f[config, "xformers"] = f
-                time_b[config, "xformers"] = b
 
             print(f"### causal={causal}, headdim={headdim}, batch_size={batch_size}, seqlen={seqlen} ###")
             for method in methods:
